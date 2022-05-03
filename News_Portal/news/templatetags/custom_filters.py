8db959a79df38ef210d170b8e7dc6d31573ae796
Sergey Bachurin <ba4urin.sergey2016@yandex.ru>
2022-05-03 15:43:53 +0500

    Обновил фильтр censor

--- News_Portal/news/templatetags/custom_filters.py
+++ News_Portal/news/templatetags/custom_filters.py
@@ -5,5 +5,20 @@ register = template.Library()
 
 @register.filter()
 def censor(text_to_filter):
-    filtered_text = 'None shall pass'
+    # В тексте text_to_filter заменим все буквы слов из списка bad_words на
+    # символы '*'. Слово из списка bad_words может являться частью другого
+    # слова. В данный момент мы все равно произведем замену.
+    # Пока, чтобы проверить работу, будем заменять часто встречающиеся предлоги
+    # и приставки
+
+    bad_words = [
+        'на',
+        'не',
+        'ли',
+        'это',
+    ]
+
+    filtered_text = text_to_filter
+    for bw in bad_words:
+        filtered_text = filtered_text.replace(bw, '*' * len(bw))
     return filtered_text
