73e6de6cc312b270525a087521a1bd3f49ffc425
Sergey Bachurin <ba4urin.sergey2016@yandex.ru>
2022-05-03 11:27:30 +0500

    Добавил вывод даты загрузки новости в формате 'день.месяц.год'

--- News_Portal/news/views.py
+++ News_Portal/news/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
@@ -22,3 +24,8 @@ class NewsDetail(DetailView):
     model = Post
     template_name = 'news_detail.html'
     context_object_name = 'news_detail'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['publication_date'] = self.object.creation_time.strftime('%d.%m.%Y')
+        return context
