e5062ae5971404c8f0c4e0e4ee0a1f93ebbe21ed
Sergey Bachurin <ba4urin.sergey2016@yandex.ru>
2022-05-11 17:01:05 +0500

    Добавил удаление поста

--- News_Portal/news/urls.py
+++ News_Portal/news/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
-from .views import NewsList, NewsDetail, NewsSearch, PostCreate, PostEdit
+from .views import (NewsList, NewsDetail, NewsSearch,
+                    PostCreate, PostEdit, PostDelete)
 
 
 urlpatterns = [
-   path('', NewsList.as_view()),
+   path('', NewsList.as_view(), name='news_list'),
    path('<int:pk>', NewsDetail.as_view(), name='news_details'),
    path('search/', NewsSearch.as_view()),
    path('create/', PostCreate.as_view()),
    path('<int:pk>/edit/', PostEdit.as_view()),
+   path('<int:pk>/delete/', PostDelete.as_view()),
 ]
--- News_Portal/news/views.py
+++ News_Portal/news/views.py
@@ -2,7 +2,9 @@ from datetime import datetime
 
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.urls import reverse_lazy
+from django.views.generic import (ListView, DetailView, CreateView,
+                                  UpdateView, DeleteView)
 
 from .models import Post
 from .filters import NewsFilter
@@ -62,3 +64,9 @@ class PostEdit(UpdateView):
     model = Post
     form_class = PostForm
     template_name = 'post_create.html'
+
+
+class PostDelete(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('news_list')
