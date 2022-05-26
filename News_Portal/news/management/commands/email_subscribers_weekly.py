3ccfed3ef9c0cfa9b3e95195c46b91c800eefe80
Sergey Bachurin <ba4urin.sergey2016@yandex.ru>
2022-05-26 16:05:11 +0500

    Установил для задачи рассылки email периодичность: каждую неделю

--- News_Portal/news/management/commands/email_subscribers_weekly.py
+++ News_Portal/news/management/commands/email_subscribers_weekly.py
@@ -90,7 +90,7 @@ class Command(BaseCommand):
 
         scheduler.add_job(
             my_job,
-            trigger=CronTrigger(second="*/10"),  # Every 10 seconds
+            trigger=CronTrigger(week="*"),  # Every week
             id="my_job",  # The `id` assigned to each job MUST be unique
             max_instances=1,
             replace_existing=True,
