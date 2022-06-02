8e466c43df60942fa35f0d3b6bb36587b0e15769
Sergey Bachurin <ba4urin.sergey2016@yandex.ru>
2022-06-02 15:23:09 +0500

    Добавил настройки для еженедельной рассылки email с помощью celery подписчикам

--- News_Portal/news/tasks.py
+++ News_Portal/news/tasks.py
@@ -3,6 +3,7 @@ import time
 
 from .models import Post
 from .signals import notify_subscribers_new_post
+from .management.commands.email_subscribers_weekly import my_job
 
 
 @shared_task
@@ -10,3 +11,10 @@ def email_subscribers_new_post(post_id):
     # Для того чтобы проверить работу celery, пока используем функцию
     # отправки сообщения, которую мы подключали к сигналам записи в базу
     notify_subscribers_new_post(Post.objects.get(pk=post_id), 'post_add')
+
+
+@shared_task
+def email_subscribers_posts_for_week():
+    # Для того чтобы проверить работу celery, пока используем функцию
+    # отправки сообщений подписчикам с новостями за прошедшую неделю
+    my_job()
